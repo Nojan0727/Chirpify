e0bbec2bcb3ad8d1c82cffe446d4b9ce2e76c05a
Nojan0727 <9025261@student.zadkine.nl>
2025-02-20 09:17:01 +0100

    added post.php

--- Partials/header.php
+++ Partials/header.php
@@ -20,8 +20,7 @@ session_start();
         <ul>
             <li><a href="index.php">Home</a></li>
             <?php if (isset($_SESSION['user'])): ?>
-                <li><a href="profile.php">Profile</a></li>
-                <li><a href="logout.php">Logout</a></li>
+
             <?php else: ?>
                 <li><a href="login.php">Login</a></li>
                 <li><a href="register.php">Sign Up</a></li>
